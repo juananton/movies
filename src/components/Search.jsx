6c806f49f27adddc667be5cd81dc62e6715a221b
juananton <juananton.correo@gmail.com>
2023-08-08 17:14:53 +0200

    Implement data fetching

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,11 @@
 const Search = ({ query, setQuery }) => {
   return (
     <input
-      placeholder={'Search movies...'}
-      value={query}
-      onChange={e => setQuery(e.target.value)}
       className='search'
       type='text'
+      placeholder='Search movies...'
+      value={query}
+      onChange={e => setQuery(e.target.value)}
     />
   );
 };
